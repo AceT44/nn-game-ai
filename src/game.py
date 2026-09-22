e1071aa1918807c41f107b82e25da021deb37b09
AceT44 <kohei20071129@gmail.com>
2026-09-22 12:25:15 +0800

    debugging test

--- src/game.py
+++ src/game.py
@@ -3,16 +3,38 @@ import pygame
 
 class Game:
     def __init__(self):
-        pass
+        self.players = Player()
+        self.obstacles = Obstacle()
+
+        self.screen = pygame.display.set_mode((700, 700))
+        pygame.display.set_caption('Game AI')
+
+        self.running = True
+        self.clock = pygame.time.Clock()
 
     def run(self):
-        root = pygame.display.set_mode((700, 700))
-        running = True
-        clock = pygame.time.Clock()
-
-        while running:
+        while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    self.running = False
 
-            clock.tick(60)
+            self.screen.fill('white')
+
+            pygame.draw.rect(self.screen, 'black', self.players.player)
+
+            pygame.display.update()
+
+            self.clock.tick(60)
+
+
+class Player:
+    def __init__(self):
+        self.player = pygame.Rect(x=20, y=670, width=30, height=30)
+
+    def jump(self):
+        pass
+
+
+class Obstacle:
+    def __init__(self):
+        pass
