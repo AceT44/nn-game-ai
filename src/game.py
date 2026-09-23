6d2f0e3793aa642debe6535d6af6ccfeaec72fdf
AceT44 <kohei20071129@gmail.com>
2026-09-23 16:25:29 +0800

    updated obstacles

--- src/game.py
+++ src/game.py
@@ -16,6 +16,7 @@ class Game:
         self.obstacle = Obstacle(self)
 
         self.running = True
+        self.score = 0
         self.clock = pygame.time.Clock()
 
     def run(self):
@@ -27,7 +28,12 @@ class Game:
                 if event.type == pygame.KEYDOWN:
                     if event.key in (pygame.K_SPACE, pygame.K_UP) and self.player.jumping == False:
                         self.player.jump()
+
             self.player.update()
+            self.obstacle.update()
+
+            self.check_collision()
+            self.get_score()
 
             self.screen.fill('white')
 
@@ -46,15 +52,36 @@ class Game:
 
             self.clock.tick(60)
 
+    def check_collision(self):  # could be improved with stricter collision detection
+        if self.player.player_rect.colliderect(self.obstacle.obstacle_rect):
+            if self.player.vel_y > 0 and self.player.player_rect.y < self.obstacle.obstacle_rect.y:
+                self.player.player_rect.bottom = self.obstacle.obstacle_rect.top
+                self.player.vel_y = 0
+                self.player.jumping = False
+            else:
+                self.running = False
+
+    def get_score(self):
+        if self.obstacle.passed_obstacle == True:
+            self.score += 1
+            print(self.score)
+
 
 class Player:
-    JUMP_VEL = 40
-    GRAVITY = 2
+    JUMP_VEL = 17
+    GRAVITY = 0.8
+    PLAYER_WIDTH = 50
+    PLAYER_HEIGHT = 50
 
     def __init__(self, game):
         self.game = game
 
-        self.player_rect = pygame.Rect(20, 550, 50, 50)
+        self.player_rect = pygame.Rect(
+            20,
+            550,
+            self.PLAYER_WIDTH,
+            self.PLAYER_HEIGHT
+        )
         self.vel_y = 0
         self.jumping = False
 
@@ -76,4 +103,14 @@ class Obstacle:
     def __init__(self, game):
         self.game = game
 
-        self.obstacle_rect = pygame.Rect(500, 400, 100, 200)
+        self.vel_x = -3
+        self.obstacle_rect = pygame.Rect(800, 500, 50, 100)
+        self.passed_obstacle = False
+
+    def update(self):
+        self.passed_obstacle = False
+        self.obstacle_rect.x += self.vel_x
+
+        if self.obstacle_rect.right < 0:
+            self.passed_obstacle = True
+            self.obstacle_rect.x = self.game.GAME_WIDTH
